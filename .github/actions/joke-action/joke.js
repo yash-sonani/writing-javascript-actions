d9e2194d628a451f894c6aa9616bf999e4ecf4db
yash.sonani <yash.sonani@travelport.com>
2021-10-26 09:44:33 -0500

    Correct joke js

--- .github/actions/joke-action/joke.js
+++ .github/actions/joke-action/joke.js
@@ -1,18 +1,19 @@
 const request = require("request-promise");
 
 const options = {
-    method: "GET",
-    uri: "https://icanhazdadjoke.com/",
-    headers:{
-        Accept: "aplication/json",
-        "User-Agent":"Wrting JavaScript action GitHub Learning Lab Course. Visit lab.github.com or to contact us."
-    },
-    json: true
+  method: "GET",
+  uri: "https://icanhazdadjoke.com/",
+  headers: {
+    Accept: "application/json",
+    "User-Agent":
+      "Writing JavaScript action GitHub Learning Lab course.  Visit lab.github.com or to contact us."
+  },
+  json: true
 };
 
-async function getJoke(){
-    const res = await request(options);
-    return res.joke;
+async function getJoke() {
+  const res = await request(options);
+  return res.joke;
 }
 
 module.exports = getJoke;
